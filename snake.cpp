6642f5317c57e3640ad48807e2abb7f55394d580
ptakj <ptakjakub004@gmail.com>
2026-05-25 09:45:13 +0200

    GameLogic class begin

--- snake.cpp
+++ snake.cpp
@@ -1,2 +1,32 @@
-//Oled 1306  128x64 - 32x16
+//Oled 1306  128x64 - 32x16 16 collumns, 32 rows
 
+
+
+#include<iostream>
+#include<time.h>
+#include<stdllib.h>
+
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+};
+
+class GameLogic {
+    private:
+    const int ROWS = 16;
+    const int COLLUMNS = 32;
+    bool SnakeState[512];
+    int apple;
+    public:
+
+    int calculateCollumn(int position){
+    return position%COLLUMNS == 0 ? 
+    }
+
+    
+
+
+};
